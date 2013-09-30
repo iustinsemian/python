ba66ecb9cba9a7bedb4667c2fea803ca0248417d
Semjus <jstn_semian@yahoo.com>
2013-09-30 21:54:09 +0300

    first python program

---
+++ hello.py
@@ -0,0 +1,20 @@
+
+print "My name is Ius\n"
+print "Enter your name\n"
+
+
+print "Enter 2 nr."
+a=int( raw_input(""))
+b=int( raw_input(""))
+print "Sum = {0}".format(a+b)
+
+def salutare(a):
+	print "Hello {0}".format(a)
+
+salutare("iustin");
+salutare("vlad");
+
+
+def adunare(a,b):
+	print "Sum is {0}".format(a+b)
+adunare(2,3);
